af7e9945af00da35050ff030e65b8c0741b7c708
pearceant <pearceant@hotmail.co.uk>
2022-10-05 17:35:09 +0100

    changed ports to 5000

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def update_fun():
         return redirect(url_for("update_fun"))
 
 if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port=8000)
+    app.run(debug=True,host='0.0.0.0',port=5001)
